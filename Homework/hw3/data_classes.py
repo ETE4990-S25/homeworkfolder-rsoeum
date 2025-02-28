b628d1f37e009115fed1951a06a4b3af7fb514f0
Richard Soeum <rsoeum@gmail.com>
2025-02-27 20:32:39 -0800

    changed file name

--- Homework/hw3/data_classes.py
+++ Homework/hw3/data_classes.py
@@ -1,5 +1,3 @@
-import json
-
 class Person:
 
     def __init__ (self, age, name, email):
@@ -9,14 +7,10 @@ class Person:
 
 
     
-#inherits from person
+
 class Student(Person):
     def __init__(self, name, age, email, StudentId):
         self.name =name
         self.age=age
         self.email =email
         self.StudentId=StudentId
-
-
-    def Json(self):
-
