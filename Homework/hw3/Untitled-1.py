a3ebe7fde46fb8613f51ca6b69860fec119aeede
Richard Soeum <rsoeum@gmail.com>
2025-02-27 19:46:56 -0800

    created classes for person and Student

--- Homework/hw3/Untitled-1.py
+++ Homework/hw3/Untitled-1.py
@@ -1,7 +1,16 @@
-class Person(object):
+class Person:
 
-def __init__ (self, age, name, email):
+    def __init__ (self, age, name, email):
+        self.name =name
+        self.age=age
+        self.email =email
+
+
     
 
 class Student(Person):
-    
+    def __init__(self, name, age, email, StudentId):
+        self.name =name
+        self.age=age
+        self.email =email
+        self.StudentId=StudentId
